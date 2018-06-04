b25bef243e360dc44880fd7b8c5669019281f35b
Markus Hinz <markus.hinz@tum.de>
2018-06-04 12:38:57 +0200

    Linus' database URI

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 
 //Configuration variables
 const port      = process.env.PORT        || '3000';
-const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27017/moviedb';
+//const mongoURI  = process.env.MONGODB_URI || 'mongodb://localhost:27017/moviedb';
+const mongoURI  = process.env.MONGODB_URI || 'mongodb://seba:sebamaster123@linusmichel.ddns.net:27017/moviedb';
 const JwtSecret = process.env.JWT_SECRET  ||'very secret secret';
 
 module.exports = {
